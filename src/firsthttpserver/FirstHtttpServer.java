db66321b378b783bc3a6f75bb27845c83c41e555
jones <jones@10.50.137.239>
2015-02-06 11:31:50 +0100

    Added Worlds simplest Web-Server

--- src/firsthttpserver/FirstHtttpServer.java
+++ src/firsthttpserver/FirstHtttpServer.java
@@ -1,5 +1,5 @@
-package firsthttpserver;
 
+package firsthttpserver;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -12,63 +12,78 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.util.Scanner;
-
 /**
  * @author Lars Mortensen
  */
 public class FirstHtttpServer {
-  static int port = 8081;
-  static String ip = "127.0.0.1";
-  static String contentFolder ="public/";
-  public static void main(String[] args) throws Exception {
-    if(args.length == 2){
-      port = Integer.parseInt(args[0]);
-      ip = args[1];
+    static int port = 8081;
+    static String ip = "127.0.0.1";
+    static String contentFolder = "public/";
+    public static void main(String[] args) throws Exception {
+        if (args.length == 2) {
+            port = Integer.parseInt(args[0]);
+            ip = args[1];
+        }
+        HttpServer server = HttpServer.create(new InetSocketAddress(ip, port), 0);
+        server.createContext("/welcome", new RequestHandler());
+        server.createContext("/files", new SimpleFileHandler());
+        server.createContext("/headers", new RequestHandlerHeaders());
+        server.createContext("/pages", new RequestHandlerPages());
+        
+        server.setExecutor(null); // Use the default executor
+        server.start();
+        System.out.println("Server started, listening on port: " + port);
     }
-    HttpServer server = HttpServer.create(new InetSocketAddress(ip,port), 0);
-    server.createContext("/welcome", new RequestHandler());
-    server.createContext("/files", new SimpleFileHandler());
-    server.createContext("/headers", new RequestHandlerHeaders());
-    server.setExecutor(null); // Use the default executor
-    server.start();
-    System.out.println("Server started, listening on port: "+port);
-  }
-
-  static class RequestHandler implements HttpHandler {
-    @Override
-    public void handle(HttpExchange he) throws IOException {
-     String response;
-   //  response= "<h1>Welcome to my very first almost home made Web Server :-)</h1>";
-      StringBuilder sb = new StringBuilder();
-sb.append("<!DOCTYPE html>\n");
-sb.append("<html>\n");
-sb.append("<head>\n");
-sb.append("<title>My fancy Web Site</title>\n");
-sb.append("<meta charset='UTF-8'>\n");
-sb.append("</head>\n");
-sb.append("<body>\n");
-sb.append("<h2>Welcome to my very first home made Web Server123 :-)</h2>\n");
-sb.append("</body>\n");
-sb.append("</html>\n");
-response = sb.toString();
-      response += "<br>"+"URI:"+he.getRequestURI();
-      
-      Scanner scan = new Scanner(he.getRequestBody());
-      while(scan.hasNext())
-      {
-          response += "<br /> post paramter "+scan.nextLine();
-      }
-    
-      Headers h = he.getResponseHeaders();
-      h.add("Content-Type", "text/html");
-      he.sendResponseHeaders(200, response.length());
-      try (PrintWriter pw = new PrintWriter(he.getResponseBody())) {
-        pw.print(response); //What happens if we use a println instead of print --> Explain
-      }
+    static class RequestHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange he) throws IOException {
+            String response;
+            //String response = "<h1>Welcome to my very first almost home made Web Server :-)</h1>";
+            StringBuilder sb = new StringBuilder();
+            sb.append("<!DOCTYPE html>\n");
+            sb.append("<html>\n");
+            sb.append("<head>\n");
+            sb.append("<title>My fancy Web Site</title>\n");
+            sb.append("<meta charset='UTF-8'>\n");
+            sb.append("</head>\n");
+            sb.append("<body>\n");
+            sb.append("<h2>Welcome to my very first home made Web Server :-)</h2>\n");
+            sb.append("</body>\n");
+            sb.append("</html>\n");
+            response = sb.toString();
+      //response += "<br>"+"URI:"+he.getRequestURI();
+            //he.getResponseHeaders();  lav en løkke om en tabel hvor man udskriver
+            //new
+            Scanner scan = new Scanner(he.getRequestBody());
+            while (scan.hasNext()) {
+                response += "<br/>From Post form: " + scan.nextLine();
+            }
+            Headers h = he.getResponseHeaders();
+            h.add("Content-Type", "text/html");
+            he.sendResponseHeaders(200, response.length());
+            try (PrintWriter pw = new PrintWriter(he.getResponseBody())) {
+                pw.print(response); //What happens if we use a println instead of print --> Explain
+            }
+        }
     }
-  }
-  
-   static class RequestHandlerHeaders implements HttpHandler {
+    static class SimpleFileHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange he) throws IOException {
+            File file = new File(contentFolder + "index.html");
+            byte[] bytesToSend = new byte[(int) file.length()];
+            try {
+                BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+                bis.read(bytesToSend, 0, bytesToSend.length);
+            } catch (IOException ie) {
+                System.out.println(ie);
+            }
+            he.sendResponseHeaders(200, bytesToSend.length);
+            try (OutputStream os = he.getResponseBody()) {
+                os.write(bytesToSend, 0, bytesToSend.length);
+            }
+        }
+    }
+    static class RequestHandlerHeaders implements HttpHandler {
     @Override
     public void handle(HttpExchange he) throws IOException {
      String response;
@@ -118,24 +133,34 @@ response = sb.toString();
       }
     }
   }
-  
-  static class SimpleFileHandler implements HttpHandler {
-    @Override
-    public void handle(HttpExchange he) throws IOException {
-      File file = new File(contentFolder + "index.html");
-      byte[] bytesToSend = new byte[(int) file.length()];
-      try {
-        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
-        bis.read(bytesToSend, 0, bytesToSend.length);
-      } catch (IOException ie) {
-        System.out.println(ie);
-      }
-      he.sendResponseHeaders(200, bytesToSend.length);
-      try (OutputStream os = he.getResponseBody()) {
-        os.write(bytesToSend, 0, bytesToSend.length);
-      }
+
+    
+    static class RequestHandlerPages implements HttpHandler {
+        
+        @Override
+        public void handle(HttpExchange he) throws IOException {
+        String contentFolder =  "public/";
+        
+            File file = new File(contentFolder+"index.html");
+            byte[] bytesToSend = new byte[(int) file.length()];
+            try {
+                BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+                bis.read(bytesToSend, 0, bytesToSend.length);
+            } catch (IOException ie) {
+                ie.printStackTrace();
+            }
+            
+            he.sendResponseHeaders(200, bytesToSend.length);
+            try (OutputStream os = he.getResponseBody()) {
+                os.write(bytesToSend, 0, bytesToSend.length);
+            }
+        }
     }
-  }
-  
-  
+    
+    
+    
+    
+    
+    
+    
 }
